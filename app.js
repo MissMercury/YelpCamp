8d9c5ee360a0ec8a8f7b521a8338fa22456d2351
MissMercury <hannegrosemans@gmail.com>
2020-05-29 19:57:37 +0200

    var url added

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var express = require("express"),
 var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
-
-mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useUnifiedTopology: true})
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp"
+mongoose.connect(purl, {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use(bodyParser.urlencoded(
     { extended: true }
