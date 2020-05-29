a3f90d3b1a8fffa70844732eaf8774a2d59cafdc
MissMercury <hannegrosemans@gmail.com>
2020-05-29 19:17:44 +0200

    MongoDB Atlas added

--- app.js
+++ app.js
@@ -17,7 +17,14 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-hanne:Murphy1988app.js@clusteryelp-0grbu.mongodb.net/test?retryWrites=true&w=majority", {
+     useNewUrlParser: true, 
+     useUnifiedTopology: true
+    }).then(() => {
+        console.log("Connected to DB");
+    }).catch(err => {
+        console.log("ERROR:", err.message);
+    })
 
 app.use(bodyParser.urlencoded(
     { extended: true }
